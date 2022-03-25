fe3ccb77cc94b9ffbd8bd33f773ab2e8aacfa4e9
khofele <karen.hofele@gmx.de>
2022-03-25 10:09:38 +0100

    [added] weapons

--- Assets/Resources/InfinityPBR/Editor/SFB_DisplayPolycount.cs
+++ Assets/Resources/InfinityPBR/Editor/SFB_DisplayPolycount.cs
@@ -5,7 +5,7 @@ using UnityEditor;
 
 public class SFB_DisplayPolycount : MonoBehaviour {
 
-	[MenuItem("Window/Infinity PBR/Polycount %#p")]
+	[MenuItem("Window/Infinity PBR/Polycount")]
 	static void ShowPolycount(){
 		int totalTriCount = 0;
 		int totalVertCount = 0;
