47ad8d0c36b7256c4e9adfb23d53d099f5d8d324
khofele <karen.hofele@gmx.de>
2022-03-25 09:53:03 +0100

    [added] dragon assets

---
+++ Assets/InfinityPBR/_InfinityPBR - Dragons/Scripts/SFB_DragonDemo.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFB_DragonDemo : MonoBehaviour {
+
+	Animator animator;
+	public GameObject[] Heads;
+	public GameObject[] Backs;
+	public GameObject[] Tails;
+
+	void Start(){
+		animator = GetComponent<Animator> ();
+	}
+	
+	public void SuperRandom()
+	{
+		for (int i = 0; i < Heads.Length; i++)
+		{
+			Heads[i].SetActive(false);
+		}
+		for (int i = 0; i < Backs.Length; i++)
+		{
+			Backs[i].SetActive(false);
+		}
+		for (int i = 0; i < Tails.Length; i++)
+		{
+			Tails[i].SetActive(false);
+		}
+		
+		Heads[Random.Range(0, Heads.Length)].SetActive(true);
+		Backs[Random.Range(0, Backs.Length)].SetActive(true);
+		Tails[Random.Range(0, Tails.Length)].SetActive(true);
+	}
+
+	public void UpdateLocomotion(float value){
+		animator.SetFloat ("locomotion", value);
+	}
+}
---
+++ Assets/InfinityPBR/_InfinityPBR - Dragons/Scripts/SFB_DragonFirebreath.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFB_DragonFirebreath : MonoBehaviour {
+
+	public GameObject[] magicSpell;
+
+	public void StartSpell(){
+		for (int i = 0; i < magicSpell.Length; i++){
+			if (magicSpell [i].GetComponent<ParticleSystem> ()) {
+				ParticleSystem ms = magicSpell [i].GetComponent<ParticleSystem> ();
+				var em = ms.emission;
+				em.enabled = true;
+
+			} else if (magicSpell [i].GetComponent<Light> ()) {
+				magicSpell [i].GetComponent<Light> ().enabled = true;
+			}
+		}
+	}
+
+	public void EndSpell(){
+		for (int i = 0; i < magicSpell.Length; i++){
+			if (magicSpell [i].GetComponent<ParticleSystem> ()) {
+				ParticleSystem ms = magicSpell [i].GetComponent<ParticleSystem> ();
+				var em = ms.emission;
+				em.enabled = false;
+			} else if (magicSpell [i].GetComponent<Light> ()) {
+				magicSpell [i].GetComponent<Light> ().enabled = false;
+			}
+		}
+	}
+}
