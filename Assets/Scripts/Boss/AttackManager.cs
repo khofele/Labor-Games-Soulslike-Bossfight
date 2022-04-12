2e52f2a4c7d8418f75a454402292a6235779a8d2
khofele <karen.hofele@gmx.de>
2022-04-12 16:20:18 +0200

    [added] attack manager

---
+++ Assets/Scripts/Boss/AttackManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackManager : MonoBehaviour
+{
+    // all attacks
+    private Attack attackFront = new Attack(10, 20);
+    private Attack attackLeft = new Attack(10, 20);
+    private Attack attackRight = new Attack(10, 20);
+    private Attack attackFireHead = new Attack(10, 20);
+
+    private Attack currentAttack = null;
+
+    public Attack CurrentAttack { get => currentAttack; set => currentAttack = value; }
+    public Attack AttackFront { get => attackFront; }
+    public Attack AttackLeft { get => attackLeft; }
+    public Attack AttackRight { get => attackRight; }
+    public Attack AttackFireHead { get => attackFireHead; }
+}
