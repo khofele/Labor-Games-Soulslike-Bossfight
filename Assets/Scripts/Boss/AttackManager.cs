dcf7f22d2c32394fbda439f2ba27c90417d2cada
khofele <karen.hofele@gmx.de>
2022-04-17 16:56:43 +0200

    [added] new attacks

--- Assets/Scripts/Boss/AttackManager.cs
+++ Assets/Scripts/Boss/AttackManager.cs
@@ -9,14 +9,16 @@ public class AttackManager : MonoBehaviour
     private Attack attackLeft = new Attack(100, 150);
     private Attack attackRight = new Attack(100, 150);
     private Attack attackFireHead = new Attack(150, 250);
+    private Attack attackWideFireHead = new Attack(200, 250);
+    private Attack attackFly = new Attack(100, 150);
 
     private Attack currentAttack = null;
-    [SerializeField] private Animator animator = null;
 
     public Attack CurrentAttack { get => currentAttack; set => currentAttack = value; }
     public Attack AttackFront { get => attackFront; }
     public Attack AttackLeft { get => attackLeft; }
     public Attack AttackRight { get => attackRight; }
     public Attack AttackFireHead { get => attackFireHead; }
-    public Animator Animator { get => animator; }
+    public Attack AttackWideFireHead { get => attackWideFireHead; }
+    public Attack AttackFly { get => attackFly; }
 }
