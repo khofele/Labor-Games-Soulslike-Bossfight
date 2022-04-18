1a889b49328ba1267b9b318fe31e5c8755b3a8dc
khofele <karen.hofele@gmx.de>
2022-04-18 14:41:08 +0200

    [added] stun (fly)

---
+++ Assets/Scripts/Boss/Actions/FlyStunned.cs
@@ -0,0 +1,28 @@
+using BBUnity.Actions;
+using Pada1.BBCore;
+using Pada1.BBCore.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Action("FlyStunned")]
+public class FlyStunned : GOAction
+{
+    [InParam("attack manager")]
+    [SerializeField] private AttackManager attackManager = null;
+
+    private BossController bossController = null;
+
+    public override void OnStart()
+    {
+        bossController = gameObject.GetComponent<BossController>();
+    }
+
+    public override TaskStatus OnUpdate()
+    {
+        bossController.Animator.SetTrigger(""); // TODO Animations Task
+        // TODO: Stun Fly
+        Debug.Log("boss fly stunned");
+        return TaskStatus.COMPLETED;
+    }
+}
