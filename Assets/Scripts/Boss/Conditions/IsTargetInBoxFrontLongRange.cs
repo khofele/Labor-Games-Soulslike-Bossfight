9e33bfaeae66efb9875b52aa88903f9528f6eedd
khofele <karen.hofele@gmx.de>
2022-05-15 19:58:38 +0200

    [fixed] ranges

--- Assets/Scripts/Boss/Conditions/IsTargetInBoxFrontLongRange.cs
+++ Assets/Scripts/Boss/Conditions/IsTargetInBoxFrontLongRange.cs
@@ -18,7 +18,7 @@ public class IsTargetInBoxFrontLongRange : GOCondition
 
     public override bool Check()
     {
-        Collider[] objectsInRange = Physics.OverlapBox(new Vector3(gameObject.transform.position.x, (gameObject.transform.position.y + 2.5f), gameObject.transform.position.z + 10), new Vector3(gameObject.transform.localScale.x + 8, gameObject.transform.localScale.y + 5, gameObject.transform.localScale.z + 15), gameObject.transform.rotation);
+        Collider[] objectsInRange = Physics.OverlapBox(new Vector3(gameObject.transform.position.x, (gameObject.transform.position.y + 2.5f), gameObject.transform.position.z + 16.5f), new Vector3(gameObject.transform.localScale.x + 3, gameObject.transform.localScale.y + 5, gameObject.transform.localScale.z + 15), gameObject.transform.rotation);
 
         for (int i = 0; i < objectsInRange.Length; i++)
         {
--- Assets/Scripts/Boss/Conditions/IsTargetInBoxLeft.cs
+++ Assets/Scripts/Boss/Conditions/IsTargetInBoxLeft.cs
@@ -15,7 +15,7 @@ public class IsTargetInBoxLeft : GOCondition
 
     public override bool Check()
     {
-        Collider[] objectsInRange = Physics.OverlapBox(new Vector3(gameObject.transform.position.x - 5, gameObject.transform.position.y + 5, gameObject.transform.position.z + 1.5f), new Vector3(gameObject.transform.localScale.x + 6, gameObject.transform.localScale.y, gameObject.transform.localScale.z + 13), gameObject.transform.rotation);
+        Collider[] objectsInRange = Physics.OverlapBox(new Vector3(gameObject.transform.position.x - 5.7f, gameObject.transform.position.y + 2.5f, gameObject.transform.position.z + 2.5f), new Vector3(gameObject.transform.localScale.x + 3, gameObject.transform.localScale.y + 5, gameObject.transform.localScale.z + 13), gameObject.transform.rotation);
 
         for (int i = 0; i < objectsInRange.Length; i++)
         {
