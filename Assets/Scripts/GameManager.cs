358382227ede1586769493f5d4680838e6d50575
TaLeibl <tleibl@stud.hs-heilbronn.de>
2022-05-15 13:30:42 +0200

    [changed] GameManager: invisible and locked mouse cursor

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,8 +8,15 @@ public class GameManager : MonoBehaviour
     [SerializeField] private CharController player = null;
     [SerializeField] private bool phaseTwo = false;
 
+    private void Start()
+    {
+        //set mouse cursor invisible and lock in game screen
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
     private void Update()
     {
+        //invoke boss phase two
         if(boss.Health >= (boss.Health * 0.5f))
         {
             phaseTwo = true;
