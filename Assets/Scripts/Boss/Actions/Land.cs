8b7c766f09a419b57594b9961e5c430091b05643
khofele <karen.hofele@gmx.de>
2022-05-17 16:32:23 +0200

    [fixed] landing animation

--- Assets/Scripts/Boss/Actions/Land.cs
+++ Assets/Scripts/Boss/Actions/Land.cs
@@ -18,10 +18,6 @@ public class Land : GOAction
     public override TaskStatus OnUpdate()
     {
         bossController.Animator.SetTrigger("Land");
-        for(int i = 0; i < 500; i++)
-        {
-            gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y-0.1f, gameObject.transform.position.z);
-        }
         bossController.FlyTimer.StartTimer();
         bossController.IsFlying = false;
         return TaskStatus.COMPLETED;
