88fcf1225361de6c33d854f1fd4ada106e66d9cb
khofele <karen.hofele@gmx.de>
2022-05-18 20:36:32 +0200

    [fixed] flying mechanic + phase 2

---
+++ Assets/Scripts/Boss/Reset.cs
@@ -0,0 +1,29 @@
+using stateMachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reset : BaseState
+{
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.ResetTrigger("Attack 1");
+        animator.ResetTrigger("StunnedStanding");
+        animator.ResetTrigger("Fire Head 1");
+        animator.ResetTrigger("Walk");
+        animator.ResetTrigger("Hit 1");
+        animator.ResetTrigger("Hit 2");
+        animator.ResetTrigger("TakeOff");
+        animator.ResetTrigger("StunnedFlying");
+        animator.ResetTrigger("AttackFly");
+        animator.ResetTrigger("BreatheMagicFire");
+        animator.ResetTrigger("BreathePoisonFire");
+        animator.ResetTrigger("BreatheBasicFire");
+        animator.ResetTrigger("Hit 3");
+        animator.ResetTrigger("AttackFront");
+        animator.ResetTrigger("Attack 2");
+        animator.ResetTrigger("Land");
+
+
+    }
+}
