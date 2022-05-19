d26c88edb4ad5946433d0c70a0132d0f6a3a9dcb
khofele <karen.hofele@gmx.de>
2022-05-19 22:31:30 +0200

    [added] ui fields, enable methods, disable methods

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -4,15 +4,81 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject healthBarChar = null;
+    [SerializeField] private GameObject healthBarBoss = null;
+    [SerializeField] private GameObject staminaBar = null;
+    [SerializeField] private GameObject potionField = null;
+    [SerializeField] private GameObject winMessage = null;
+    [SerializeField] private GameObject deathMessage = null;
+    [SerializeField] private GameObject bossName = null;
+    
+    public void EnableHealthBarChar()
     {
-        
+        healthBarChar.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void DisableHealthBarChar()
     {
-        
+        healthBarChar.SetActive(false);
+    }
+
+    public void EnableHealthBarBoss()
+    {
+        healthBarBoss.SetActive(true);
+    }
+
+    public void DisableHealthBarBoss()
+    {
+        healthBarBoss.SetActive(false);
+    }
+
+    public void EnableStaminaBar()
+    {
+        staminaBar.SetActive(true);
+    }
+
+    public void DisableStaminaBar()
+    {
+        staminaBar.SetActive(false);
+    }
+
+    public void EnablePotionField()
+    {
+        potionField.SetActive(true);
+    }
+
+    public void DisablePotionField()
+    {
+        potionField.SetActive(false);
+    }
+
+    public void EnableWinMessage()
+    {
+        winMessage.SetActive(true);
+    }
+
+    public void DisableWinMessage()
+    {
+        winMessage.SetActive(false);
+    }
+
+    public void EnableDeathMessage()
+    {
+        deathMessage.SetActive(true);
+    }
+
+    public void DisableDeathMessage()
+    {
+        deathMessage.SetActive(false);
+    }
+
+    public void EnableBossName()
+    {
+        bossName.SetActive(true);
+    }
+
+    public void DisableBossName()
+    {
+        bossName.SetActive(false);
     }
 }
