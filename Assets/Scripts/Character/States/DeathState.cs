078cca32bd47a672db69a5ee7bed998cd2268660
TaLeibl <tleibl@stud.hs-heilbronn.de>
2022-06-16 10:10:40 +0200

    [fix] stop reg stamina when character dead

--- Assets/Scripts/Character/States/DeathState.cs
+++ Assets/Scripts/Character/States/DeathState.cs
@@ -14,19 +14,10 @@ namespace stateMachine
 
             //sound
             GetAudioPlayer(animator).PlayDeath();
+
+            //stop stamina reg
+            GetCharController(animator).SetRegStamina(false);
         }
-
-        // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-        //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-        //{
-            
-        //}
-
-        // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-        //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-        //{    
-
-        //}
     }
 
 }
